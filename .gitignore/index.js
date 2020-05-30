4db034a33dd2c1a3e56480c2d99a0845c3b44521
LeBancal <62794843+LeBancal@users.noreply.github.com>
2020-05-30 20:43:25 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function(){
+
+    bot.user.setActivity('Pong')
+    .catch(console.error)
+})
+bot.on('message', function (message){
+    if (message.content === '!ping'){
+        message.channel.send('pong')
+    }
+})
+
+bot.on('guildmemberAdd', function (member){
+    member.createDM().then(function (channel){
+        return channel.send('Bienvenue sur le serveur ' + member.displayName)
+        .catch(console.error)
+    })
+
+})
+
+
+bot.login('NzE2MjEzNzA2NDc5ODk0NTM4.XtIkqQ.DRWOCECMytgRwb2WEMZrXBFcCKA')
